3f386898c6f409d30c787eb2611a5e081a5a6e16
Artem Sokolov <artem.sokolov@gmail.com>
2016-08-08 13:17:39 -0400

    Added a bias term to the model

--- demix.R
+++ demix.R
@@ -5,29 +5,32 @@
 
 #' Constrained non-negative matrix factorization
 #'
-#' Learns a heterogeneous sample mixture model of the form x = Bw,
+#' Learns a heterogeneous sample mixture model of the form x = Bw + b,
 #'  where x is a p-by-1 column vector representing the sample,
-#'  B is a p-by-m matrix of m basis vectors and w is a m-by-1 vector of
-#'  mixture coefficients. The model is subject to several constraints, which
-#'  are outlined below.
+#'  B is a p-by-m matrix of m basis vectors, w is a m-by-1 vector of
+#'  mixture coefficients, and b is a bias term. The model is subject to
+##  several constraints, which are outlined below.
 #'
 #' The model is subject to non-negativity contraints and
 #'  the \sum_j w_j = 1 convexity contraint.
 #'
 #' Furthermore, we assume that only the first m-1 bases are specified and
 #'  the model is to learn the m^th basis. However, it is also assumed that
-#'  w_m is known, and we must learn w_i for i = 1, ..., (m-1)
+#'  w_m is known, and we must learn w_i for i = 1, ..., (m-1). The bias term
+#'  b is also learned.
 #'
 #' @param x A p-by-1 vector of the sample in p dimensions
 #' @param Bm1 A p-by-(m-1) matrix of the first (m-1) basis vectors
 #' @param wm The m^th entry of the mixture coeffcient vector
 #' @param nIter Number of training iterations
+#' @param fix.bias If TRUE, the bias term is fixed at 0
 #' @return A list with two elements:
 #' \describe{
 #'   \item{B}{the fully specified p-by-m matrix of basis vectors}
 #'   \item{w}{the fully specified m-by-1 vector of mixture coefficients}
+#'   \item{b}{the bias term in the model}
 #' }
-consNMF <- function( x, Bm1, wm, nIter=1000 )
+consNMF <- function( x, Bm1, wm, nIter=1000, fix.bias=FALSE )
 {
     ## Verify dimensionality
     p <- length(x)
@@ -43,10 +46,11 @@ consNMF <- function( x, Bm1, wm, nIter=1000 )
     B <- cbind( Bm1, 0 )
     f <- (1-wm) / (m-1)		## Start with equal contribution fractions
     w <- c( rep( f, m-1 ), wm )
+    b <- 0
 
     ## Define the objective function
     f.obj <- function()
-    { r <- x - B %*% w; sqrt(mean( r * r )) }
+    { r <- x - B %*% w - b; sqrt(mean( r * r )) }
     
     cat( "Initial RMSE :", f.obj(), "\n" )
 
@@ -54,19 +58,28 @@ consNMF <- function( x, Bm1, wm, nIter=1000 )
     {
         ## Estimate the missing basis
         u <- B[,-m] %*% w[-m]
-        B[,m] <- (x-u) / w[m]
+        B[,m] <- (x-b-u) / w[m]
         j <- which( B[,m] < 0 )
         B[j,m] <- 0
 
+#        cat( "RMSE after estimating the missing basis:", f.obj(), "\n" )
+
         ## Re-estimate the mixture coefficients
-        y <- x - B[,m] * w[m]
+        y <- x - B[,m] * w[m] - b
         w[-m] <- convreg( B[,-m], y, 1 - w[m] )
+
+#        cat( "RMSE after estimating the mixture coefficients:", f.obj(), "\n" )
+
+        ## Estimate the bias term
+        if( fix.bias == FALSE )
+            b <- mean( x - B %*% w )
+#        cat( "RMSE after estimating the bias term:", f.obj(), "\n" )
 
         if( iter %% as.integer(nIter / 5) == 0 )
             cat( "RMSE after iteration", iter, ":", f.obj(), "\n" )
     }
     
-    list( B=B*p, w=w )
+    list( B=B*p, w=w, b=b*p )
 }
 
 #' Linear regression with non-negative and convex constraints.
